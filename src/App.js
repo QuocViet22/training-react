fa0204279b8862382f3713f237552abd222181c1
unknown <your name>
2023-01-03 16:10:26 +0700

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+}
 
 function App() {
   return (
-    <Menu />
+    <Menu dishes={this.state.dishes} />
   );
 }
 
